ac1125fb3c786da3adf5d17b02174a325c94713e
Jamie Phan <21326604@student.uwa.edu.au>
2017-10-06 15:22:56 +0800

    Add package resources to ship connection settings

--- DustyPyManager/stream/Dynamo.py
+++ DustyPyManager/stream/Dynamo.py
@@ -1,8 +1,36 @@
+"""Dynamo Stream Class
+"""
+
+import configparser
+import pkg_resources
 import boto3
+
+def get_kls_config(ini_path=None):
+    """Gets pkg_resources for Class instances (in abscence of user preference)
+
+    Args:
+        - ini_path (str, optional): path to configuration file, if not set the
+                                    default from the pkg_resources is used.
+
+    Returns:
+        configparser object
+    """
+
+    if (ini_path is None):
+        ini_path = pkg_resources.resource_string('DustyPyManager.resources.stream', 'DynamoConnection.ini')
+
+    config = configparser.ConfigParser()
+    config.read_string(ini_path.decode('ascii'))
+
+    return config
+
 
 def test():
     
-    client = boto3.client('dynamodb', region_name='ap-southeast-1')
+    conf = get_kls_config()
+    region_name = conf['default']['region']
+
+    client = boto3.client('dynamodb', region_name=region_name)
 
 if __name__ == '__main__':
     test()
