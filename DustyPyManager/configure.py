7fc6ba45650bf99b66b73f1c1df8f481c318aa99
Jamie Phan <21326604@student.uwa.edu.au>
2017-10-10 22:32:58 +0800

    Configuration Handling done at TLD of pacakge
    The configuration module will handle all user settings of the
    DustyPyManager app. This will primarily rely on the resources
    that are shipped with pkg_resources, however it should check
    for user input

---
+++ DustyPyManager/configure.py
@@ -0,0 +1,35 @@
+"""
+Handles all configuration of DustyPyManager
+"""
+
+import pkg_resources
+import configparser
+
+def get_config_resource(resource_pkg, resource_name):
+    """Gets Configuration object for resource requested
+
+    Args:
+        - resource_pkg (str): The name of the resource sub-package
+        - resource_name (str): The name of the ini file (with extension)
+
+    Returns:
+        configparser object
+    """
+        
+    resource_pkg = resource_pkg.strip()
+
+    #Check the pkg exists in resources
+    if resource_pkg not in ['stream',
+                            'wsn']:
+        raise FileNotFoundError("Pkg Resource %s does not exist" % resource_pkg)        
+    
+    pkg_full = 'DustyPyManager.resources.%s' % resource_pkg
+    ini_path = pkg_resources.resource_string(pkg_full,
+                                             resource_name)
+    # TODO: Handle exception if resource is not found
+    # TODO: Handle missing extension
+
+    config = configparser.ConfigParser()
+    config.read_string(ini_path.decode('ascii'))
+
+    return config
--- DustyPyManager/stream/Dynamo.py
+++ DustyPyManager/stream/Dynamo.py
@@ -1,34 +1,14 @@
 """Dynamo Stream Class
 """
 
-import configparser
 import pkg_resources
 import boto3
 
-def get_kls_config(ini_path=None):
-    """Gets pkg_resources for Class instances (in abscence of user preference)
-
-    Args:
-        - ini_path (str, optional): path to configuration file, if not set the
-                                    default from the pkg_resources is used.
-
-    Returns:
-        configparser object
-    """
-
-    if (ini_path is None):
-        ini_path = pkg_resources.resource_string('DustyPyManager.resources.stream',
-                                                 'DynamoConnection.ini')
-
-    config = configparser.ConfigParser()
-    config.read_string(ini_path.decode('ascii'))
-
-    return config
-
+from DustyPyManager import configure
 
 def test():
     
-    conf = get_kls_config()
+    conf = configure.get_config_resource('stream', 'DynamoConnection.ini')
     region_name = conf['default']['region']
 
     db = boto3.resource('dynamodb')
